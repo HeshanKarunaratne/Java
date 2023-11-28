8aa702c3f6ac1bc7c89dad82d854625b90ebd7e5
HeshanKarunaratne <heshan947102@gmail.com>
2023-11-28 18:59:37 +0530

    ClimbingStairs

---
+++ src/hackerrankquestions/ClimbingStairs.java
@@ -0,0 +1,28 @@
+package hackerrankquestions;
+
+/**
+ * @author Heshan Karunaratne
+ */
+public class ClimbingStairs {
+
+    public static int climbStairs(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+}
