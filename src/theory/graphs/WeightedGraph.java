b9ab796d008559938dbb60cc2473ac6fb8007ebb
HeshanKarunaratne <heshan947102@gmail.com>
2024-02-20 10:54:01 +0530

    Graphs

--- src/theory/graphs/WeightedGraph.java
+++ src/theory/graphs/WeightedGraph.java
@@ -35,24 +35,3 @@ public class WeightedGraph {
     }
 
 }
-
-class DemoGraph3 {
-    public static void main(String[] args) {
-        WeightedGraph weightedGraph = new WeightedGraph(5);
-        list.addVertex(1);
-        list.addVertex(2);
-        list.addVertex(3);
-        list.addVertex(4);
-        list.addVertex(5);
-
-        list.addEdge(1, 4);
-        list.addEdge(1, 3);
-        list.addEdge(1, 2);
-        list.addEdge(3, 5);
-        list.addEdge(2, 3);
-
-        list.dfsIterative(1);
-        System.out.println("");
-        list.bfsIterative(1);
-    }
-}
