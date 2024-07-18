d782edf99a5b0d7c33ace09151b6a36f3c2c3666
HeshanKarunaratne <heshan947102@gmail.com>
2024-07-18 10:23:23 +0530

    BestTimeToBuyAndSellStock

--- src/dsa/arrays/BestTimeToBuyAndSellStock.java
+++ src/dsa/arrays/BestTimeToBuyAndSellStock.java
@@ -1,13 +1,32 @@
 package dsa.arrays;
 
+/*
+121. Best Time to Buy and Sell Stock
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ */
+
 /**
  * @author Heshan Karunaratne
  */
 public class BestTimeToBuyAndSellStock {
-
-
     public static void main(String[] args) {
-        int[] prices = {7, 2, 5, 4, 3, 1};
+        int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(maxProfit(prices));
     }
 
@@ -33,3 +52,15 @@ public class BestTimeToBuyAndSellStock {
     }
 
 }
+
+/*
+
+You buy a stock in any given day and try to maximize the profit by selling it on another day(selling day > buying day)
+
+prices    | 7 | 1 | 5 | 3 | 6 | 4 |
+
+buyPrice  | 7 | 1 | 1 | 1 | 1 | 1 |
+profit    | 0 | 0 | 4 | 4 | 5 | 5 |
+maxProfit | 0 | 0 | 4 | 4 | 5 | 5 |
+
+ */
