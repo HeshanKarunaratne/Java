c366ee93c68b21b8a5f7f65e59e3cbf9768f6243
HeshanKarunaratne <heshan947102@gmail.com>
2024-08-04 17:16:35 +0530

    Candy

--- src/dsa/arrays/Candy.java
+++ src/dsa/arrays/Candy.java
@@ -1,5 +1,29 @@
 package dsa.arrays;
 
+/*
+135. Candy
+
+There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+Return the minimum number of candies you need to have to distribute the candies to the children.
+
+Example 1:
+Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively
+
+Example 2:
+Input: ratings = [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.
+
+ */
+
 /**
  * @author Heshan Karunaratne
  */
@@ -36,3 +60,11 @@ public class Candy {
         return candyCount;
     }
 }
+
+/*
+Candies    12  4  3  11  34  34  1  67
+Left        1  2  1   2   3   1  1   2
+Right       3  2  1   1   1   2  1   1
+Max         3  2  1   2   3   2  1   2
+Need 15 candies
+ */
