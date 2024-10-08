caf84318063cbc3a7372e184b5e1bfde82d34f19
HeshanKarunaratne <heshan947102@gmail.com>
2024-10-08 09:54:50 +0530

    NQueens2

--- src/dsa/backtracking/NQueens2.java
+++ src/dsa/backtracking/NQueens2.java
@@ -1,61 +1,64 @@
 package dsa.backtracking;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author Heshan Karunaratne
  */
 public class NQueens2 {
-    public static int totalNQueens(int n) {
-        char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
-            }
-        }
-
-        List<List<String>> res = new ArrayList<>();
-        int leftRow[] = new int[n];
-        int upperDiagonal[] = new int[2 * n - 1];
-        int lowerDiagonal[] = new int[2 * n - 1];
-        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
-        return res.size();
-    }
-
-    static void solve(int col, char[][] board, List<List<String>> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
-        if (col == board.length) {
-            res.add(construct(board));
-            return;
-        }
-
-        for (int row = 0; row < board.length; row++) {
-            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
-                leftRow[row] = 1;
-                lowerDiagonal[row + col] = 1;
-                upperDiagonal[board.length - 1 + col - row] = 1;
-                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
-                board[row][col] = '.';
-                leftRow[row] = 0;
-                lowerDiagonal[row + col] = 0;
-                upperDiagonal[board.length - 1 + col - row] = 0;
-            }
-        }
-    }
-
-    static List<String> construct(char[][] board) {
-        List<String> res = new LinkedList<>();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
-        }
-        return res;
-    }
 
     public static void main(String[] args) {
         int i = totalNQueens(4);
         System.out.println(i);
+    }
+
+    public static int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        solve(res, n, 0, board);
+        return res.size();
+    }
+
+    static void solve(List<List<String>> res, int n, int row, char[][] board) {
+        if (n == row) {
+            res.add(construct(board));
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (canPlaceQueen(board, row, col)) {
+                board[row][col] = 'Q';
+                solve(res, n, row + 1, board);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private static boolean canPlaceQueen(char[][] board, int row, int col) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    static List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (char[] chars : board) {
+            res.add(new String(chars));
+        }
+        return res;
     }
 }
