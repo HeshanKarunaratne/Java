f52ddfe25337b0207b021994034ea9f79eea248d
HeshanKarunaratne <heshan947102@gmail.com>
2024-10-21 13:24:36 +0530

    TwoSum2, ValidPalindrome

--- src/dsa/two_pointers/TwoSum2.java
+++ src/dsa/two_pointers/TwoSum2.java
@@ -1,5 +1,30 @@
 package dsa.two_pointers;
 
+/*
+167. Two Sum II - Input Array Is Sorted
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+Your solution must use only constant extra space.
+
+Example 1:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+Example 2:
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+Example 3:
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ */
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,9 +35,9 @@ import java.util.Map;
 public class TwoSum2 {
 
     public static void main(String[] args) {
-        int[] arr = {-1,0};
-        int[] output = twoSum(arr, -1);
-        System.out.println(Arrays.toString(output));
+        int[] arr = {2, 3, 4};
+        System.out.println(Arrays.toString(twoSum(arr, 6)));
+        System.out.println(Arrays.toString(twoSumV2(arr, 6)));
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -24,8 +49,35 @@ public class TwoSum2 {
             if (map.containsKey(complement)) {
                 return new int[]{map.get(complement), i + 1};
             }
-            map.put(nums[i] , i + 1);
+            map.put(nums[i], i + 1);
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSumV2(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[]{};
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int sum = nums[start] + nums[end];
+
+            if (sum == target) {
+                return new int[]{start + 1, end + 1};
+            } else if (sum > target) {
+                end--;
+            } else {
+                start++;
+            }
         }
         return new int[]{};
     }
 }
+
+/*
+Keep two pointers to the start and the end
+If the sum of those pointers matches the target you found the indexes
+Otherwise move the pointers to the left and right till the sum matches the target
+
+ */
--- src/dsa/two_pointers/ValidPalindrome.java
+++ src/dsa/two_pointers/ValidPalindrome.java
@@ -1,5 +1,29 @@
 package dsa.two_pointers;
 
+/*
+125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ */
+
 /**
  * @author Heshan Karunaratne
  */
@@ -7,9 +31,8 @@ public class ValidPalindrome {
 
     public static void main(String[] args) {
         String s = "A man, a plan, a canal: Panama";
-//        String s = "race a car";
-//        String s = "ab_a";
         System.out.println(isPalindrome(s));
+        System.out.println(isPalindromeV2(s));
     }
 
     public static boolean isPalindrome(String s) {
@@ -25,4 +48,35 @@ public class ValidPalindrome {
         return filteredString.equals(reversedString);
     }
 
+    public static boolean isPalindromeV2(String s) {
+        if (s.isBlank()) return true;
+
+        s = s.toLowerCase();
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start <= end) {
+            char cStart = s.charAt(start);
+            char cEnd = s.charAt(end);
+
+            if (!Character.isLetterOrDigit(cStart)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(cEnd)) {
+                end--;
+            } else {
+                if (cStart != cEnd) return false;
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
 }
+
+/*
+If the string is empty then return true
+Use the Characters static function isLetterOrDigit to identify whether the given char is actually a letter or a number
+
+Move the pointers from left if it is not a valid character
+Move the pointers from right if it is not a valid character
+ */
