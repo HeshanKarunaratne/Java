1663a58290e8736721b96bfa81de770718ea6d02
HeshanKarunaratne <heshan947102@gmail.com>
2025-09-05 01:30:58 +0530

    TwoSum js

--- src/dsa/hashmap/TwoSum.js
+++ src/dsa/hashmap/TwoSum.js
@@ -12,7 +12,27 @@ const findTwoSumBruteForce = (nums, target) => {
     return null;
 }
 
+const findTwoSumOptimized = (nums, target) => {
+    const numMap = new Map();
+    for (let p1 = 0; p1 < nums.length; p1++) {
+        const numberToFind = target - nums[p1];
+
+        if (numMap.has(numberToFind)) {
+            return [numMap.get(numberToFind), p1];
+        }
+        numMap.set(nums[p1], p1);
+    }
+    return null;
+}
+
+const nums = [1, 3, 7, 9, 2, 4, 6, 5, 8, 0, 20]
+const target = 28;
 const start = performance.now();
-const result = findTwoSumBruteForce([1, 3, 7, 9, 2], 11);
+const result = findTwoSumBruteForce(nums, target);
 const end = performance.now();
-console.log(`Result:[${result}] - Execution in: ${(end - start).toFixed(3)} ms`);
+console.log(`findTwoSumBruteForce:[${result}] - Execution in: ${(end - start).toFixed(6)} ms`);
+
+const start2 = performance.now();
+const result2 = findTwoSumOptimized(nums, target);
+const end2 = performance.now();
+console.log(`findTwoSumOptimized:[${result}] - Execution in: ${(end2 - start2).toFixed(6)} ms`);
